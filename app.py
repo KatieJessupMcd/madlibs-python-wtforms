d53a49558dcfe8e72feda8e60c2900ddb938042c
KatieJessupMcD <katie.jessupmcd@gmail.com>
2019-03-15 11:27:54 -0700

    rendered basic story form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
-from forms import forms
+from forms import StoryForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,8 +9,20 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.route('/')
-def render_form(): 
+def render_form():
+    """Render questions for madlibs form"""
 
-@app.route('/story')
-def render_story():
-    
+    form = StoryForm()
+
+    if form.validate_on_submit():
+        place = form.place.data
+        noun = form.noun.data
+        verb = form.verb.data
+        adjective = form.adjective.data
+        plural_noun = form. plural_noun.data
+        return f"These are the things in the form: {place}, {noun}, {verb}, {adjective}, {plural_noun}"
+    else:
+        return render_template("questions_form.html", form=form)
+
+# @app.route('/story')
+# def render_story():
